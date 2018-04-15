0b29fdd09a1f9fc50d13b741cda9716bd849e088
Owen Kellie-Smith <owen.kelliesmith@gmail.com>
2018-04-16 00:42:06 +0100

    Separate tests

--- phpunit-tests/Form_Test.php
+++ phpunit-tests/Form_Test.php
@@ -10,21 +10,55 @@ class Form_Test extends PHPUnit_Framework_TestCase
   public function tearDown(){
   }
   
-  public function test_FormWithDatabaseLength(){
-    $this->assertTrue( strlen( getCopySetForm() ) >= 0  );
+  public function test_FormWithDatabaseLength1(){
     $this->assertTrue( strlen( getDeleteSetForm() ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength2(){
     $this->assertTrue( strlen( getCopySetForm() ) > 0 );
-    $this->assertTrue( strlen( getDeleteSetForm() ) > 0 );
+  }
+
+  public function test_FormWithDatabaseLength3(){
     $this->assertTrue( strlen( getEfileForm() ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength4(){
     $this->assertTrue( strlen( getEditNoteForm() ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength5(){
     $this->assertTrue( strlen( getEditSetForm() ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength6(){
     $this->assertTrue( strlen( getGigSetForm( 1 ) ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength7(){
     $this->assertTrue( strlen( getNewNoteForm() ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength8(){
     $this->assertTrue( strlen( getPartForm(1) ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength9(){
     $this->assertTrue( strlen( getPeople() ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength10(){
     $this->assertTrue( strlen( getPublicationForm() ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength11(){
     $this->assertTrue( strlen( getSetPartsForm() ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength12(){
     $this->assertTrue( strlen( getSetPartsOutput( 1, 'dummy') ) >= 0 );
+  }
+
+  public function test_FormWithDatabaseLength13(){
     $this->assertTrue( strlen( getSongs() ) >= 0 );
   }
 
