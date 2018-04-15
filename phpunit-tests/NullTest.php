399f54e430ceadc93cc19ceb59e29da59563a196
Owen Kellie-Smith <owen.kelliesmith@gmail.com>
2018-04-15 22:54:51 +0100

    PHPUNIT

---
+++ phpunit-tests/NullTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class My_Test extends PHPUnit_Framework_TestCase
+{
+  private $debug = false;
+  
+  public function setup(){
+  }
+
+  public function tearDown(){
+	}
+  
+
+  public function test_nothing(){
+    $this->assertTrue( true );
+  }  
+
+
+}
+
